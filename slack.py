b0b4dfe7fff82cbc6a631de3e88e4c267fb84c01
Pranav2612000 <pranav2000joglekar@gmail.com>
2024-04-26 15:45:47 +0530

    feat: fetch usernames for all users

--- slack.py
+++ slack.py
@@ -12,6 +12,86 @@ MAX_SCROLLS = 5000
 def log(data):
     print(data)
 
+def delay(browser, time):
+    browser.execute_script('''
+        const delay = async (time) => {
+            await new Promise((resolve, _) => {
+                setTimeout(() => {
+                resolve(true);
+                }, time);
+            });
+        };
+    ''' + f'delay({time})')
+
+def navigateToChannel(browser, channelName):
+    browser.execute_script('''
+        async function navigateToChannel(channelName) {
+            const channels = document.querySelectorAll('.p-channel_sidebar__static_list__item');
+            const channel = Array.from(channels).find((channel) => channel.innerText === channelName);
+            channel.click();
+        }
+    ''' + f'navigateToChannel("{channelName}")')
+
+def openMemberListForCurrentChannel(browser):
+    browser.execute_script('''
+        async function openMemberListForCurrentChannel() {
+            const openMemberListBtn = document.querySelector('.p-avatar_stack--details');
+
+            openMemberListBtn.click();
+        };
+    ''' + 'openMemberListForCurrentChannel();')
+
+def getNumberOfUsers(browser):
+    numberOfUsers = browser.execute_script('''
+        try {
+            const countEle = document.querySelector('.c-tabs__tab_count');
+            return Number(countEle.innerText);
+        } catch (err) {
+            return 0;
+        }
+    ''')
+    return numberOfUsers
+
+def getUserNames(browser):
+    users = browser.execute_script('''
+        namesMap = {}
+        let numberOfUsers;
+        membersListEle = document.querySelector('.p-ia_details_popover__members_list')
+        scroller = membersListEle.querySelector('.c-scrollbar__hider');
+        try {
+            const countEle = document.querySelector('.c-tabs__tab_count');
+            numberOfUsers = Number(countEle.innerText);
+        } catch {
+            numberOfUsers = 0;
+        }
+
+        while (Object.keys(namesMap).length < numberOfUsers) {
+            scroller.scrollBy({ top: 200, behavior: 'smooth'});
+
+            let currentMemberEles = membersListEle.querySelectorAll('.p-ia_details_popover__members_list_item_member')
+            for (let i = 0; i < currentMemberEles.length; i++) {
+                try {
+                    const currentMemberEle = currentMemberEles[i];
+                    const currentName = currentMemberEle.querySelector('.c-member_name').innerText;
+                    const currentMemberId = currentMemberEle.id;
+                    if (namesMap[currentMemberId] != undefined) {
+                        continue
+                    } else {
+                        namesMap[currentMemberId] = currentName;
+                    }
+                } catch {
+                }
+            }
+            await new Promise((resolve, _) => {
+                setTimeout(() => {
+                    resolve(true);
+                }, 200);
+            });
+        }
+        return namesMap;
+    ''')
+    return users
+
 try:
     with open('cookies.json', 'r') as file:
         cookies = json.load(file)
@@ -42,9 +122,24 @@ with open('cookies.json', 'w') as file:
 log('Login complete')
 
 # Go to channel
+channelName = 'development'
 browser.get("https://app.slack.com/client/TK8V2RL7Q/C01G7CFNA8K")
 
 time.sleep(10)
 
 log('Starting the script now')
 
+#browser.execute_script(f'navigateToChannel("{channelName}")')
+navigateToChannel(browser, channelName)
+log(f'Navigation to channel {channelName} completed')
+
+time.sleep(10)
+
+log('Opening member list')
+openMemberListForCurrentChannel(browser)
+time.sleep(10)
+numberOfUsers = getNumberOfUsers(browser)
+log(f'Getting names of {numberOfUsers} users...')
+users = getUserNames(browser)
+print(users)
+print(len(users))
